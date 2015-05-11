6b3699c53d17971bcd20d1030f653a8cb7b39e98
RobertaDomingues2015 <RobertaDomingues2015@users.noreply.github.com>
2015-05-11 13:59:19 -0300

    Update usuario.php

--- usuario.php
+++ usuario.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
-		<title> Pós Gradução Desesvolvimento Web - Atividade 5
-		</title>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     		<meta name="viewport" content="width=device-width, initial-scale=1">
     		<meta name="description" content="">
     		<meta name="author" content="">
+		<title> Pós Gradução Desesvolvimento Web - Atividade 5
+		</title>
+		
 		<link rel="stylesheet" href="css/estilo.css" />
 		<link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
